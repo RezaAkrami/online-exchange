7661497c3bab340a565e709491e1abcca254b47b
Reza Akrami <RezaAkrami32@gmail.com>
2022-09-21 11:09:58 +0430

    filter coin name

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import React , { useState , useEffect } from 'react';
 import { getCoin } from '../services/api';
 import Coin from './Coin';
 import Loading from './Loading';
 
 const Landing = () => {
 
+    // Define states
     const [conis, setConis] = useState([]);
+    const [search, setSearch] = useState("");
 
+    // Get data when page loaded
     useEffect(()=>{
 
         const fetchApi = async ()=>{
@@ -20,13 +21,21 @@ const Landing = () => {
 
     } , []);
 
+    //setSearch for input value
+    const searchHandler = e=>{
+        setSearch(e.target.value);
+    }
+    
+    //Search coin name
+    const searchCoin = conis.filter(coin=> coin.name.toLowerCase().includes(search.toLowerCase()) ) ;
+
     return (
         <div>
-            <input type="text" placeholder='search ...' />
+            <input type="text" placeholder='search ...' value={search} onChange={searchHandler} />
             {
                 conis.length 
                 ?
-                    conis.map( coinsData => <Coin key={coinsData.id} data={coinsData}/> )
+                    searchCoin.map( coinsData => <Coin key={coinsData.id} data={coinsData}/> )
                 :
                     <Loading/>
             }
